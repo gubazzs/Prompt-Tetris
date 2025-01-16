2ce6ec6dbd8fa8aebbdac9ed49d592b4fc5b90ce
Gabriel Ribeiro Marques <100245422+gubazzs@users.noreply.github.com>
2025-01-15 23:54:29 -0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,21 +1,5 @@
-import os, time
-from src.Game import Jogo
 from src.Menu import menu
 
-
-
 while True:
-  
-  game = Jogo()
-  os.system('cls')
-  
-  if menu() == 0:
-    os.system('cls')
-    time.sleep(2)
-    game.jogar()
-  elif menu() == 1:
-    print("holding")
-  elif menu() == 2:
-    break
-
-os._exit()
+    if menu() == 2:
+      break
